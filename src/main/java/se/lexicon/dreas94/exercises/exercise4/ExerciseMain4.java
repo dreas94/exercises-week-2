9cf40d810b40704a9f3570f668126c840cfa10e4
Andreas Eriksson <dreas94@gmail.com>
2022-03-28 13:58:53 +0200

    Completed Exercise 4

--- src/main/java/se/lexicon/dreas94/exercises/exercise4/ExerciseMain4.java
+++ src/main/java/se/lexicon/dreas94/exercises/exercise4/ExerciseMain4.java
@@ -1,5 +1,4 @@
 package se.lexicon.dreas94.exercises.exercise4;
-
 import java.util.Arrays;
 
 public class ExerciseMain4
@@ -9,7 +8,18 @@ public class ExerciseMain4
         final int[] array1 = new int[]{1,2,3,4,5,6,7,8,9,10};
         final int[] array2 = Arrays.copyOf(array1,array1.length);
 
-        System.out.println("Elements from first array: " + Arrays.toString(array1));
-        System.out.println("Elements from second array: " + Arrays.toString(array2));
+        System.out.println("Elements from first array: " + arrayToString(array1));
+        System.out.println("Elements from second array: " + arrayToString(array2));
+    }
+
+    private String arrayToString(int[] array)
+    {
+        String arrayString = "";
+
+        for(int num : array)
+        {
+            arrayString = arrayString.concat(num + " ");
+        }
+        return arrayString;
     }
 }
