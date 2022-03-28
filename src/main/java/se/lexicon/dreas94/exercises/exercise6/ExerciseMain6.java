8a10968d520766181a48a44ec015dbdc7342fa46
Andreas Eriksson <dreas94@gmail.com>
2022-03-28 15:22:28 +0200

    Completed Exercise 6

--- src/main/java/se/lexicon/dreas94/exercises/exercise6/ExerciseMain6.java
+++ src/main/java/se/lexicon/dreas94/exercises/exercise6/ExerciseMain6.java
@@ -1,9 +1,20 @@
 package se.lexicon.dreas94.exercises.exercise6;
 
+import java.text.DecimalFormat;
+
 public class ExerciseMain6
 {
     public void runExercise()
     {
+        final double[] array = new double[]{43, 5, 23, 17, 2, 14};
 
+        double totalSum = 0.0;
+        for(double num : array)
+        {
+            totalSum += num;
+        }
+        DecimalFormat df = new DecimalFormat("###.#");
+
+        System.out.println("Average is: " + df.format(totalSum/(double)array.length));
     }
 }
