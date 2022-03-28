4bbd471564e4f34adfa8797f8f510981c70ecc17
Andreas Eriksson <dreas94@gmail.com>
2022-03-28 15:49:20 +0200

    Completed Exercise 7

--- src/main/java/se/lexicon/dreas94/exercises/exercise7/ExerciseMain7.java
+++ src/main/java/se/lexicon/dreas94/exercises/exercise7/ExerciseMain7.java
@@ -4,6 +4,29 @@ public class ExerciseMain7
 {
     public void runExercise()
     {
+        final int[] array = new int[10];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 4;
+        array[3] = 7;
+        array[4] = 9;
+        array[5] = 12;
 
+        System.out.println("Array: " + arrayToString(array,true));
+        System.out.println("Odd Array: " + arrayToString(array,false));
+    }
+
+    private String arrayToString(int[] array, boolean showEven)
+    {
+        String arrayString = "";
+
+        for(int num : array)
+        {
+            if(num != 0 && ((num % 2 != 0) || showEven))
+            {
+                arrayString = arrayString.concat(num + " ");
+            }
+        }
+        return arrayString;
     }
 }
