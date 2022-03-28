a92ec20f288abeb2fed62d08774692091bb3dda6
Andreas Eriksson <dreas94@gmail.com>
2022-03-28 13:43:01 +0200

    Completed Exercise 4

--- src/main/java/se/lexicon/dreas94/exercises/exercise4/ExerciseMain4.java
+++ src/main/java/se/lexicon/dreas94/exercises/exercise4/ExerciseMain4.java
@@ -1,9 +1,15 @@
 package se.lexicon.dreas94.exercises.exercise4;
 
+import java.util.Arrays;
+
 public class ExerciseMain4
 {
     public void runExercise()
     {
+        final int[] array1 = new int[]{1,2,3,4,5,6,7,8,9,10};
+        final int[] array2 = Arrays.copyOf(array1,array1.length);
 
+        System.out.println("Elements from first array: " + Arrays.toString(array1));
+        System.out.println("Elements from second array: " + Arrays.toString(array2));
     }
 }
