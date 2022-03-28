4223d6376ddaa13fb512351cac01f79ca018836c
Andreas Eriksson <dreas94@gmail.com>
2022-03-28 13:58:59 +0200

    Completed Exercise 5

--- src/main/java/se/lexicon/dreas94/exercises/exercise5/ExerciseMain5.java
+++ src/main/java/se/lexicon/dreas94/exercises/exercise5/ExerciseMain5.java
@@ -4,6 +4,25 @@ public class ExerciseMain5
 {
     public void runExercise()
     {
+        final String[][] array = new String[2][2];
+        array[0][0] = "France";
+        array[0][1] = "Paris";
+        array[1][0] = "Sweden";
+        array[1][1] = "Stockholm";
 
+        print2DStringArray(array);
+    }
+
+    private void print2DStringArray(String[][] array)
+    {
+        for(String[] strArr : array)
+        {
+            String arrayString = "";
+            for (String str : strArr)
+            {
+                arrayString = arrayString.concat(str + " ");
+            }
+            System.out.println(arrayString);
+        }
     }
 }
