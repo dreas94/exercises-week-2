81d33957cf5ec39ce142f9cdf5756c34cf98c2cf
Andreas Eriksson <dreas94@gmail.com>
2022-03-28 13:42:51 +0200

    Completed Exercise 3

--- src/main/java/se/lexicon/dreas94/exercises/exercise3/ExerciseMain3.java
+++ src/main/java/se/lexicon/dreas94/exercises/exercise3/ExerciseMain3.java
@@ -1,9 +1,13 @@
 package se.lexicon.dreas94.exercises.exercise3;
+import java.util.Arrays;
 
 public class ExerciseMain3
 {
     public void runExercise()
     {
-
+        final String[] array = new String[]{"Paris", "London", "New York", "Stockholm"};
+        System.out.println("String array: " + Arrays.toString(array));
+        Arrays.sort(array);
+        System.out.println("Sort string array:: " + Arrays.toString(array));
     }
 }
