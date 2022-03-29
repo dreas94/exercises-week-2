54880023ee3b2cd7a7b1038fca1bed4c3cdf775f
Andreas Eriksson <dreas94@gmail.com>
2022-03-29 10:46:35 +0200

    Menu Update

--- src/main/java/se/lexicon/dreas94/App.java
+++ src/main/java/se/lexicon/dreas94/App.java
@@ -45,72 +45,83 @@ public class App
 
             System.out.println("--------------------------------------------------");
 
+            boolean completed = false;
+
             switch(choice)
             {
                 case 1:
                     //Completed
                     ExerciseMain1 exercise1 = new ExerciseMain1();
-                    exercise1.runExercise();
+                    completed = exercise1.runExercise();
                     break;
                 case 2:
                     //Completed
                     ExerciseMain2 exercise2 = new ExerciseMain2();
-                    exercise2.runExercise();
+                    completed = exercise2.runExercise();
                 case 3:
                     //Completed
                     ExerciseMain3 exercise3 = new ExerciseMain3();
-                    exercise3.runExercise();
+                    completed = exercise3.runExercise();
                     break;
                 case 4:
                     //Completed
                     ExerciseMain4 exercise4 = new ExerciseMain4();
-                    exercise4.runExercise();
+                    completed = exercise4.runExercise();
                     break;
                 case 5:
                     //Completed
                     ExerciseMain5 exercise5 = new ExerciseMain5();
-                    exercise5.runExercise();
+                    completed = exercise5.runExercise();
                     break;
                 case 6:
                     //Completed
                     ExerciseMain6 exercise6 = new ExerciseMain6();
-                    exercise6.runExercise();
+                    completed = exercise6.runExercise();
                     break;
                 case 7:
                     //Completed
                     ExerciseMain7 exercise7 = new ExerciseMain7();
-                    exercise7.runExercise();
+                    completed = exercise7.runExercise();
                     break;
                 case 8:
                     //Completed
                     ExerciseMain8 exercise8 = new ExerciseMain8();
-                    exercise8.runExercise();
+                    completed = exercise8.runExercise();
                     break;
                 case 9:
+                    //Completed
                     ExerciseMain9 exercise9 = new ExerciseMain9();
-                    exercise9.runExercise();
+                    completed = exercise9.runExercise();
                     break;
                 case 10:
                     ExerciseMain10 exercise10 = new ExerciseMain10();
-                    exercise10.runExercise();
+                    completed = exercise10.runExercise();
                     break;
                 case 11:
                     ExerciseMain11 exercise11 = new ExerciseMain11();
-                    exercise11.runExercise();
+                    completed = exercise11.runExercise();
                     break;
                 case 12:
                     ExerciseMain12 exercise12 = new ExerciseMain12();
-                    exercise12.runExercise();
+                    completed = exercise12.runExercise();
                     break;
                 case 13:
                     ExerciseMain13 exercise13 = new ExerciseMain13();
-                    exercise13.runExercise();
+                    completed = exercise13.runExercise();
                     break;
                 case 0:
                     System.out.println("Goodbye!");
                     break;
+                default:
+                    completed = false;
+                    break;
             }
-            System.out.println("Exercise " + choice + " has finished. Returning to menu!");
+            if(completed)
+                System.out.println("Exercise " + choice + " has finished.");
+            else
+                System.out.println("Exercise " + choice + "has not been completed. Try again later.");
+
+            System.out.println("Returning to menu!");
         }while(choice != 0);
     }
 }
