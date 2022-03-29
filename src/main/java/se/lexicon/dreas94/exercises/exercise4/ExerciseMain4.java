82d0e3bcd0d5d8b4e7f93f09131b291a63108b41
Andreas Eriksson <dreas94@gmail.com>
2022-03-29 10:51:29 +0200

    Completed & Updated Exercise 4

--- src/main/java/se/lexicon/dreas94/exercises/exercise4/ExerciseMain4.java
+++ src/main/java/se/lexicon/dreas94/exercises/exercise4/ExerciseMain4.java
@@ -3,13 +3,15 @@ import java.util.Arrays;
 
 public class ExerciseMain4
 {
-    public void runExercise()
+    public boolean runExercise()
     {
         final int[] array1 = new int[]{1,2,3,4,5,6,7,8,9,10};
         final int[] array2 = Arrays.copyOf(array1,array1.length);
 
         System.out.println("Elements from first array: " + arrayToString(array1));
         System.out.println("Elements from second array: " + arrayToString(array2));
+
+        return true;
     }
 
     private String arrayToString(int[] array)
